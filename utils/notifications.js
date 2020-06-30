bf20ad62d39eb9f806d1ebcd8de9feaa490a5dff
unknown <fabian.kratz@protonmail.com>
2020-06-30 11:02:45 +0200

    add notifications

--- utils/notifications.js
+++ utils/notifications.js
@@ -1,22 +1,35 @@
 import { AsyncStorage } from 'react-native'
+import * as Notifications from 'expo-notifications';
+
 
 const NOTIFICATION_KEY = '@mobile-flashcards:notifications'
   
 export function setLocalNotification () {
-    console.log("setting notification")
     AsyncStorage.getItem(NOTIFICATION_KEY)
         .then(JSON.parse)
         .then((data) => {
-            console.log(data)
             if (data === null) {
-                // ask for permissions 
-                // if granted ... 
-                AsyncStorage.setItem(NOTIFICATION_KEY, JSON.stringify(true))
+                Notifications.requestPermissionsAsync().then(({status}) => {
+                    if (status === "granted") {
+                        Notifications.cancelAllScheduledNotificationsAsync().then(() => {
+                            Notifications.scheduleNotificationAsync({
+                                content: {
+                                    title: "Improve your memory!",
+                                    body: "Don't forget to complete a quiz today!"
+                                },
+                                trigger: {
+                                    seconds: 60 * 60 * 24,
+                                    repeats: true,
+                                }
+                            })
+                            AsyncStorage.setItem(NOTIFICATION_KEY, JSON.stringify(true))
+                        })
+                    }
+                })
             }
         }).catch(console.log)
 }
 export function clearLocalNotification () {
-    console.log("clearing notification")
     return AsyncStorage.removeItem(NOTIFICATION_KEY)
-        .then()
+        .then(Notifications.cancelAllScheduledNotificationsAsync)
 }
